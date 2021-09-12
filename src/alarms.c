aaa64a5203eaefa352dfce136e2679f41cd8a15f
linear cannon <dev@linear.network>
2021-09-12 15:00:21 -0700

    alarm: new subsystem
    allows for setting alarms to run callback functions. useful
    for scheduling interrupts for later without having to poll regularly.

---
+++ src/alarms.c
@@ -0,0 +1,74 @@
+
+#include <stdlib.h>
+
+#include "alarms.h"
+
+alarmManager_t * AlarmManager( AlarmChangedCallback_t AlarmChangedCallback, void * alarmChangedCallbackData ) {
+    alarmManager_t * manager = calloc( 1, sizeof(alarmManager_t) );
+    
+    if ( !manager )
+        return NULL;
+
+    manager->AlarmChangedCallback = AlarmChangedCallback;
+    manager->alarmChangedCallbackData = alarmChangedCallbackData;
+
+    return manager;
+}
+
+alarm_t * AlarmGetNext( alarmManager_t * manager ) {
+    int i;
+    alarm_t * next = NULL;
+    
+    if ( !manager )
+        return NULL;
+    
+    for ( i = 0; i < MAX_ALARMS; i++ ) {
+        alarm_t * alarm = manager->alarms[i];
+
+        if ( !next ) {
+            if ( alarm ) {
+                next = alarm;
+            }
+            continue;
+        }
+
+        if ( !alarm )
+            continue;
+
+        if ( ( alarm->when > 0 ) && ( alarm->when < next->when ) ) {
+            next = alarm;
+            continue;
+        }
+    }
+
+    return next;
+}
+
+void AlarmTimePassed( alarmManager_t * manager, int howmuch, int runCallbacks ) {
+    int i = 0;
+
+    for ( i = 0; i < MAX_ALARMS; i++ ) {
+        alarm_t * alarm = manager->alarms[i];
+        
+        if ( alarm && ( alarm->when >= 0 ) ) {
+            alarm->when = alarm->when - howmuch;
+            if ( alarm->when < 1 ) {
+                alarm->when = -1;
+                if ( runCallbacks ) {
+                    alarm->Callback( alarm->callbackData );
+                }
+            }
+        }
+    }
+}
+
+void AlarmAdd( alarmManager_t * manager, alarm_t * alarm ) {
+    int i;
+    
+    for ( i = 0; i < MAX_ALARMS; i++ ) {
+        if ( ! manager->alarms[i] ) {
+            manager->alarms[i] = alarm;
+            return;
+        }
+    }
+}
---
+++ src/alarms.h
@@ -0,0 +1,29 @@
+
+#define MAX_ALARMS 32
+
+typedef void* (AlarmChangedCallback_t)(void * data);
+typedef int (AlarmGetTimePassedCallback_t)(void * data);
+typedef unsigned long (AlarmGetFrameTimeCallback_t)(void * data);
+
+typedef struct alarm_s {
+    char * name;
+    void * callbackData;
+    void (*Callback)(void * data);
+    long when;
+} alarm_t;
+
+typedef struct alarmManager_s {
+    void * alarmChangedCallbackData;
+    void * alarmGetTimePassedCallbackData;
+    void * alarmGetFrameTimeCallbackData;
+    AlarmChangedCallback_t * AlarmChangedCallback;
+    AlarmGetTimePassedCallback_t * AlarmGetTimePassedCallback;
+    AlarmGetFrameTimeCallback_t * AlarmGetFrameTimeCallback;
+    alarm_t *alarms[32];
+} alarmManager_t;
+
+alarmManager_t * AlarmManager( AlarmChangedCallback_t AlarmChangedCallback, void * alarmChangedCallbackData );
+
+void AlarmAdd( alarmManager_t * manager, alarm_t * alarm );
+alarm_t * AlarmGetNext( alarmManager_t * manager );
+void AlarmTimePassed( alarmManager_t * manager, int howmuch, int runCallbacks );

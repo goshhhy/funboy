670857e852f1ff1cf861cbe9704d2dc72ac20897
linear cannon <dev@linear.network>
2021-09-11 22:23:34 -0700

    fix comments

--- src/device/device.h
+++ src/device/device.h
@@ -1,9 +1,9 @@
 
 
-// uncomment for 32-bit bus size
-//#define BUS_SIZE_32
+/* uncomment for 32-bit bus size */
+/* #define BUS_SIZE_32 */
 
-// uncomment for faster, but more memory intensive, 16-bit bus mapping method
+/* uncomment for faster, but more memory intensive, 16-bit bus mapping method */
 #define BUS_MAP_FAST16
 
 #ifdef BUS_SIZE_32
